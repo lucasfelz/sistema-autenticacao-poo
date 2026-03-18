a5ca25c7f3a8d5963ef43a89311d5cff4ccebd92
Lucas Silva Felz <lucasfelz@hotmail.com>
2026-03-18 05:35:46 -0300

    init: estrutura inicial do projeto

---
+++ trabalho-POO.java
@@ -0,0 +1,60 @@
+
+
+abstract class Pessoa {
+  private String nome;
+  private String CPF;
+  private String dataNascimento;
+  private String login;
+  private String senha;
+
+
+  // construtor da classe abstrata
+  public Pessoa(String nome, String CPF, String dataNascimento, String login, String senha) {
+    this.nome = nome;
+    this.CPF = CPF;
+    this.dataNascimento = dataNascimento;
+    this.login = login;
+    this.senha = senha;
+  }
+
+  // getters da classe
+  public String getNome() {
+     return this.nome;
+  }
+
+  public String getCPF() {
+    return this.CPF;
+  }
+
+  public String getDataNascimento() {
+    return this.dataNascimento;
+  }
+
+  public String getLogin() {
+    return this.login;
+  }
+
+  // verificação simples se a senha foi informada
+  public boolean verificarSenha(String senhaInformada) {
+    return this.senha.equals(senhaInformada);
+  }
+
+  // metodo abstrato para exibir os dados da pessoa. abstrato para proteger a exibição. um usuario nao ve do outro
+  public abstract void exibirPessoa();
+
+}
+
+
+  // interface de permissao; permissoes de usuarios
+  // pensei em dois metodos, acessarSistema() e exibirPermissoes()
+  public interface permissao {
+    void acessarSistema();
+    void exibirPermissoes();
+
+  }
+
+
+
+
+
+
